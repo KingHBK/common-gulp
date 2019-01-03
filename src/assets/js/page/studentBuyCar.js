fa692fe4a311a613b0b2968316bded47f7627648
KingHBK <1270369097@qq.com>
2019-01-03 15:11:34 +0800

    留学生购车页切图v1.0

---
+++ src/assets/js/page/studentBuyCar.js
@@ -0,0 +1,32 @@
+const regPhone = /^[1][3,4,5,7,8][0-9]{9}$/
+
+layui.use([ 'carousel', 'element', 'form', 'flow' ], () => {
+
+  let carousel = layui.carousel
+  let element = layui.element
+  let form = layui.form
+  let flow = layui.flow
+
+  carousel.render({
+    elem: '#showCar1',
+    width: '100%',
+    height: '485px',
+    indicator: 'none',
+    arrow: 'always'
+  })
+  carousel.render({
+    elem: '#showCar2',
+    width: '100%',
+    height: '485px',
+    indicator: 'none',
+    arrow: 'always'
+  })
+
+  form.verify({
+    checkPhone(value, item) {
+      if (!regPhone.test(value)) return '请输入正确的手机号'
+    }
+  })
+
+  flow.lazyimg()
+})
